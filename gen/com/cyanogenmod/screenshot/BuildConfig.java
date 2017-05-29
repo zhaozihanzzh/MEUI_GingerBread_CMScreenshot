5eed8b4168daaf4f768dc9296aad2912879bbab0
zhaozihanzzh <zhaozihanrxh@163.com>
2017-05-29 23:32:29 +0800

    Use Settings.System instead of getting MEUIRomCtrl's SharedPreference

--- gen/com/cyanogenmod/screenshot/BuildConfig.java
+++ gen/com/cyanogenmod/screenshot/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.cyanogenmod.screenshot;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = false;
+    public final static boolean DEBUG = true;
 }
--- src/com/cyanogenmod/screenshot/ScreenshotActivity.java
+++ src/com/cyanogenmod/screenshot/ScreenshotActivity.java
@@ -1,27 +1,4 @@
-/*
-**
-** Copyright 2010, Koushik Dutta
-** Copyright 2011, The CyanogenMod Project 
-**
-** Licensed under the Apache License, Version 2.0 (the "License"); 
-** you may not use this file except in compliance with the License. 
-** You may obtain a copy of the License at 
-**
-**       http://www.apache.org/licenses/LICENSE-2.0 
-**
-** Unless required by applicable law or agreed to in writing, software 
-** distributed under the License is distributed on an "AS IS" BASIS, 
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-** See the License for the specific language governing permissions and 
-** limitations under the License.
-*/
-/* Copyright 2016-2017 ,Me Creation Team
-@Modifier zhaozihanzzh
-@Email zhaozihanrxh@163.com
-*/
-
 package com.cyanogenmod.screenshot;
-
 import android.app.*;
 import android.content.*;
 import android.content.pm.*;
@@ -37,16 +14,36 @@ import android.view.*;
 import android.widget.*;
 import java.io.*;
 
+/**
+ *
+ * Copyright 2010, Koushik Dutta
+ * Copyright 2011, The CyanogenMod Project 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * Copyright 2016-2017, Me Creation Team
+ * @modifier zhaozihanzzh
+ * @email zhaozihanrxh@163.com
+ */
+
 public class ScreenshotActivity extends Activity
 {
     private static Bitmap mBitmap = null;
     private Handler mHander = new Handler();
     private String mScreenshotFile;
-	private Context meui;
-	private SharedPreferences meuiPf;
+	private ContentResolver CR;
 	private static final String TAG=ScreenshotActivity.class.getSimpleName();
 	
-    private static /*final*/ String mSSBucketName =
+    private static String mSSBucketName =
         Environment.getExternalStorageDirectory().toString()
         + "/DCIM/Screenshots";
 
@@ -55,22 +52,12 @@ public class ScreenshotActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-		//获取MEUI自定义设置
-		try
-		{
-			meui=createPackageContext("com.meui.RomCtrl", Context.CONTEXT_IGNORE_SECURITY);
-
-			meuiPf=meui.getSharedPreferences("com.meui.RomCtrl_preferences",Context.MODE_WORLD_READABLE);
-		}
-		catch (PackageManager.NameNotFoundException e)
-		{
-			Log.d(TAG,"Couldn't find control app:"+e.toString());
-		}
-        finally{
-		final String PATH=meuiPf.getString("path",mSSBucketName);
-		if(PATH!=""){
+		CR=getContentResolver();
+		String PATH=Settings.System.getString(CR,"path");
+		if(PATH=="")PATH=mSSBucketName;
+		
          mSSBucketName =Environment.getExternalStorageDirectory().toString()+"/"+PATH;
-		}
+		
         if (!(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))) {
             Toast toast = Toast.makeText(ScreenshotActivity.this, getString(R.string.not_mounted), Toast.LENGTH_LONG);
             toast.show();
@@ -79,11 +66,10 @@ public class ScreenshotActivity extends Activity
         mConnection = new MediaScannerConnection(ScreenshotActivity.this, mMediaScannerConnectionClient);
         mConnection.connect();
 		// 等待
-		final int waitTime=meuiPf.getInt("screenshot_delay",1);
-			takeScreenshot(waitTime);
-    }}
-
-    void takeScreenshot()
+		final int waitTime=Settings.System.getInt(CR,"screenshot_delay",1);
+		takeScreenshot(waitTime);
+    }
+    private void takeScreenshot()
     {
         final String mRawScreenshot = String.format("%s/tmpshot.bmp", Environment.getExternalStorageDirectory().toString());
         File dir = null;
@@ -124,36 +110,28 @@ public class ScreenshotActivity extends Activity
                 matrix.postRotate((rot*-1));
                 mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), matrix, true);
             }
-
             try
             {
-                 dir = new File(mSSBucketName);
+                dir = new File(mSSBucketName);
                 if (!dir.exists()) dir.mkdirs();
 
-				String mFormat=meuiPf.getString("screenshot_format","png");
+				String mFormat=Settings.System.getString(CR,"screenshot_format");
+				if(mFormat=="")mFormat="png";
                 mScreenshotFile = String.format("%s/screenshot-%d."+mFormat, mSSBucketName, System.currentTimeMillis());
                 FileOutputStream fout = new FileOutputStream(mScreenshotFile);
                
-				int quality=meuiPf.getInt("screenshot_quality",100);
+				int quality=Settings.System.getInt(CR,"screenshot_quality",100);
 				boolean isSuccess=true;
-				if(mFormat=="png"){
-					isSuccess= mBitmap.compress(CompressFormat.PNG,quality, fout);
-				}else{
-					isSuccess= mBitmap.compress(CompressFormat.JPEG,quality, fout);
-					}
-				
+				if(mFormat=="png") isSuccess= mBitmap.compress(CompressFormat.PNG,quality, fout);
+				else isSuccess= mBitmap.compress(CompressFormat.JPEG,quality, fout);
                 fout.close();
-			if(!isSuccess){
-				Toast.makeText(this,"保存失败!",Toast.LENGTH_LONG).show();
-			}
+			    if(!isSuccess) Toast.makeText(this,"保存失败!",Toast.LENGTH_LONG).show();
 				//原来是读取CM设置中存储的SHARE_SCREENSHOT属性。
-					boolean shareScreenshot=meuiPf.getBoolean("share_screenshot",false);
+				final boolean shareScreenshot=(Settings.System.getInt(CR,"share_screenshot",0) == 1 ?true: false);
                 if (shareScreenshot) {
                     Intent intent = new Intent(Intent.ACTION_SEND);
                     intent.setType("image/"+mFormat);
-
                     intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(mScreenshotFile)));
-
                     startActivity(Intent.createChooser(intent, getString(R.string.share_message)));
                 }
             }
@@ -168,18 +146,9 @@ public class ScreenshotActivity extends Activity
                 throw new Exception("Unable to save screenshot: "+ex);
             }
         }
-        catch (Exception ex)
-        {
-            Toast toast = Toast.makeText(ScreenshotActivity.this, getString(R.string.toast_error) + " " + ex.getMessage(), Toast.LENGTH_LONG);
-            toast.show();
-        }
-		/*发送Toast
-        Toast toast = Toast.makeText(ScreenshotActivity.this, getString(R.string.toast_save_location) + " " + mScreenshotFile,Toast.LENGTH_LONG);
-        toast.show();
-		*/
-		
-
-        mConnection.scanFile(mScreenshotFile, null);
+        catch (Exception ex){ Toast.makeText(ScreenshotActivity.this, getString(R.string.toast_error) + " " + ex.getMessage(), Toast.LENGTH_LONG).show();}
+        
+		mConnection.scanFile(mScreenshotFile, null);
         mConnection.disconnect();
 		
 		//MEUI发送通知
@@ -195,10 +164,6 @@ public class ScreenshotActivity extends Activity
         final NotificationManager nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 		nm.cancel(1);// 先移除旧的通知
 		nm.notify(1,notif);
-		
-		if(uri==null){
-			Log.d(TAG,"Couldn't find image file,notif is unclickable.");
-		}
         finish();
     }
 	
@@ -208,27 +173,27 @@ public class ScreenshotActivity extends Activity
             mConnection.disconnect();
             finish();
         }
-        public void onMediaScannerConnected() {
-        }
+        public void onMediaScannerConnected() {}
     };
-
-    void takeScreenshot(final int delay)
+	
+    private void takeScreenshot(final int delay)
     {
         mHander.postDelayed(new Runnable()
         {
+			@Override
             public void run()
             {
                 takeScreenshot();
             }
         }, delay * 1000);
     }
-	/**来自网络
-	* Gets the content:// URI from the given corresponding path to a file *
-	* @param context 
-	* @param imageFile
-	* @return content Uri
-	*/
-	public Uri getImageContentUri(final Context context,final java.io.File imageFile) { 
+    /**
+	 * Gets the content:// URI from the given corresponding path to a file
+	 * @param context 
+	 * @param imageFile
+	 * @return content Uri
+	 */
+	private final Uri getImageContentUri(final Context context,final java.io.File imageFile) { 
 		final String filePath = imageFile.getAbsolutePath();
 		final Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[] { MediaStore.Images.Media._ID }, MediaStore.Images.Media.DATA + "=? ", new String[] { filePath }, null); 
 		if (cursor != null && cursor.moveToFirst()) {
@@ -238,11 +203,11 @@ public class ScreenshotActivity extends Activity
 		}
 		else {
 			if (imageFile.exists()) 
-			{ ContentValues values = new ContentValues();
+			{   ContentValues values = new ContentValues();
 				values.put(MediaStore.Images.Media.DATA, filePath);
 				return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 			}
-			else {
-				return null; } } 
-				}	
+			return null;
+		} 
+	}	
 }
